8aed7daf3138cd4685a7d947b70a88656bae2941
MrDrag0nXYT <mrdrag0nxyt@gmail.com>
2023-10-21 19:13:08 +0300

    Adding ENUM with gas types and creating class for calculating (can be changed in later)

--- src/main/java/org/example/Math.java
+++ src/main/java/org/example/Math.java
@@ -1,9 +1,11 @@
 package org.example;
 
 public class Math {
-    public static double calculateA(double volume, int temp, double density, double mass, GasType gasType, ){
+    public static double calculateA(double volume, int temp, double mass, GasType gasType){
+
+
 
         double result = 0;
-        return result;
+        return result; // я обязательно сделаю вывод сразу в return, без это переменной
     }
 }
