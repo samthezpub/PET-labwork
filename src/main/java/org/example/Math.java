52c170c8bab409eb413068f7949c6d47ae1244ad
MrDrag0nXYT <mrdrag0nxyt@gmail.com>
2023-10-21 19:51:50 +0300

    Adding ENUM with gas types and creating class for calculating (can be changed in later)

--- src/main/java/org/example/Math.java
+++ src/main/java/org/example/Math.java
@@ -1,34 +1,20 @@
 package org.example;
 
 public class Math {
-    public static double calculateA(double volume, int temp, double mass, GasType gasType){
-        double density; // плотность
+    public static double calculateA(int temp, double mass, GasType gasType){
+        double volume = 1; // объём, в литрах
 
-        switch (GasType.valueOf(String.valueOf(gasType))){
-            case Nitrogen:
-                density = 1.2506;
-                break;
-            case Argon:
-                density = 1.784;
-                break;
-            case Oxygen:
-                density = 1.429;
-                break;
-            case Neon:
-                density = 0.9002;
-                break;
-            case Fluorine:
-                density = 1.696;
-                break;
-            case NitrogenOxide:
-                density = 1.2506;
-                break;
-            case CarbonOxide:
-                density = 1.165;
-                break;
-            default:
-                density = 0;
-        }
+        // плотность, в г/м3
+        double density = switch (GasType.valueOf(String.valueOf(gasType))) {
+            case Nitrogen -> 1.2506;
+            case Argon -> 1.784;
+            case Oxygen -> 1.429;
+            case Neon -> 0.9002;
+            case Fluorine -> 1.696;
+            case NitrogenOxide -> 1.2506;
+            case CarbonOxide -> 1.165;
+            default -> 0;
+        };
 
         double result = 0;
         return result; // я обязательно сделаю вывод сразу в return, без это переменной
