9b4172046ed679babc543e6c76f93e01088ecf13
MrDrag0nXYT <mrdrag0nxyt@gmail.com>
2023-10-21 19:53:37 +0300

    Adding ENUM with gas types and creating class for calculating (can be changed in later)

--- src/main/java/org/example/Math.java
+++ src/main/java/org/example/Math.java
@@ -6,12 +6,11 @@ public class Math {
 
         // плотность, в г/м3, зависит от типа газа
         double density = switch (GasType.valueOf(String.valueOf(gasType))) {
-            case Nitrogen -> 1.2506;
+            case Nitrogen, NitrogenOxide -> 1.2506;
             case Argon -> 1.784;
             case Oxygen -> 1.429;
             case Neon -> 0.9002;
             case Fluorine -> 1.696;
-            case NitrogenOxide -> 1.2506;
             case CarbonOxide -> 1.165;
             default -> 0;
         };
