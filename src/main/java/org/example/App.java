963a3d8d00827ca94843eb0865df259c283f6ed8
Vladimir <70057256+samthezpub@users.noreply.github.com>
2023-10-21 18:51:32 +0300

    about and main forms

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,41 @@
 package org.example;
 
+import javax.swing.*;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        {
+            JFrame frame = new JFrame("Main");
+            int x = 700;
+            int y = 500;
+            frame.setSize(x, y);
+            frame.setResizable(false);
+            JButton jButton = new JButton();
+            frame.setVisible(true); //Otherwise the frame will not show up
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
+
+        {
+            JFrame main = new JFrame("About");
+            int x = 700;
+            int y = 500;
+            // Загрузка изображения
+            ImageIcon imageIcon = new ImageIcon("src/main/resources/about/about.png");
+
+// Создание JLabel с изображением
+            JLabel imageLabel = new JLabel(imageIcon);
+            main.setSize(imageIcon.getIconWidth(), imageIcon.getIconHeight() + 40);
+            main.setResizable(false);
+
+
+            main.add(imageLabel);
+
+            main.setVisible(true); //Otherwise the frame will not show up
+            main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
+
     }
 }
