ec083233704c748534a179f00ce27fb8e3a4d464
Danil <elikaka.sad@mail.ru>
2023-10-25 22:08:52 +0300

    - Добавленная молярная масса элементов в enum GasType.

--- src/main/java/org/example/GasType.java
+++ src/main/java/org/example/GasType.java
@@ -1,24 +1,34 @@
 package org.example;
 
 public enum GasType {
-    Nitrogen("Азот"),
-    Argon("Аргон"),
-    Oxygen("Кислород"),
-    Neon("Неон"),
-    Fluorine("Фтор"),
-    NitrogenOxide("Оксид азота"),
-    CarbonOxide("Оксид углерода");
+    Nitrogen("Азот", 14.000F),
+    Argon("Аргон",39.948F ),
+    Oxygen("Кислород", 15.999F),
+    Neon("Неон", 20.179F),
+    Fluorine("Фтор", 18.998F),
+    NitrogenOxide("Оксид азота", 44.012F),
+    CarbonOxide("Оксид углерода", 44.010F);
+
+
 
     private String title;
+    private float mass;
 
-    GasType(String title) {
-        this.title = title;
-    }
+    GasType(String title, float mass) {this.title = title; this.mass = mass;}
 
     @Override
     public String toString() {
         return "GasType{" +
                 "title='" + title + '\'' +
+                ", mass=" + mass +
                 '}';
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public float getMass() {
+        return mass;
     }
 }
