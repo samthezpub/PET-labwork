85a4f3fdd771d741e9f8b898a66031c28c4530fe
Danil <elikaka.sad@mail.ru>
2023-10-27 13:30:38 +0300

    - Added new class StringSlice with method getStringSlice, which get String str and int x and returns newStr with sliced to x element

---
+++ src/main/java/org/example/StringSlice.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class StringSlice {
+    public static String getStringSlice (String str, int x){
+        String[] symbol = str.split("");
+        System.out.printf(Arrays.toString(symbol));
+        StringBuilder newStr = new StringBuilder();
+        for (int i = 0; i <x; i++){
+            newStr.append(symbol[i]);
+        }
+
+        return newStr.toString();
+    }
+
+}
